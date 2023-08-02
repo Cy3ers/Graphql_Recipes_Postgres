bada0627d5b2deaa9a4da907deee37bc6d64d0b0
Cy3ers <faizaan.talha@gmail.com>
2023-08-02 11:02:45 +0500

    Fixed errors

--- frontend/src/apollo/apollo.js
+++ frontend/src/apollo/apollo.js
@@ -1,9 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const port = process.env.PORT || 8080;
+const port = process.env.REACT_APP_PORT || 8080;
 
 const client = new ApolloClient({
   uri: `http://localhost:${port}/graphql`,
